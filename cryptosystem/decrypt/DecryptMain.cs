bb81aad04a42fb14eae40836125555c991a1f13e
reishisu <poiponn697@gmail.com>
2020-05-11 04:48:28 +0900

    ファイル構成の変更

---
+++ cryptosystem/decrypt/DecryptMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using common;
+
+/*
+ * カレントディレクトリ取得
+ * http://var.blog.jp/archives/66978870.html
+ * 文字コード判別
+ * http://namco.hatenablog.jp/entry/2017/02/05/160119
+ * バイナリファイルのIO
+ * https://qiita.com/miyamoto_works/items/637732d880defd1d437d
+ * 暗号化参考
+ * http://www.mizuko.okayama-c.ed.jp/gakka/i/H22kaken/H22h-ang.pdf
+ * 共通鍵にした裏付け
+ * https://www.nic.ad.jp/ja/materials/iw/2008/proceedings/H10/IW2008-H10-01.pdf
+ */
+
+namespace decrypt {
+
+	/// <summary>
+	///	暗号化を行うメインクラス
+	/// </summary>
+	public class DecryptMain {
+
+
+		/// <summary>
+		/// 暗号化を行うメイン関数
+		/// </summary>
+		/// <param name="args">コマンドライン引数</param>
+		private static void Main (string[] args) {
+
+			// 文字コードの設定
+			Console.OutputEncoding = Encoding.UTF8;
+			// コマンドライン引数で指定されたファイルの暗号化を行う
+			BinFileDecrypt(args[0]);
+		}
+
+
+		/// <summary>
+		/// 暗号化されたバイナリファイルの復号化を行う
+		/// </summary>
+		/// <param name="filePath">復号化する対象のファイルパス</param>
+		static void BinFileDecrypt (string filePath) {
+
+			// 暗号化ファイルの読み込み
+			using (BinaryReader br = new BinaryReader(File.OpenRead(filePath))) {
+
+				// 復号化ファイルの出力バス
+				string exportFilePath = $"{filePath}.bin";
+
+				// すでに暗号化されたファイルが存在している場合は削除する
+				if (File.Exists(exportFilePath)) File.Delete(exportFilePath);
+
+				// ファイルに書き込む
+				using (BinaryWriter file = new BinaryWriter(new FileStream(exportFilePath, FileMode.CreateNew))) {
+
+					//Console.WriteLine($"[br.BaseStream.Length] : {br.BaseStream.Length}");
+
+					// 読み込めている間コンソールに出力し続ける
+					while (br.BaseStream.Position != br.BaseStream.Length) {
+
+						long length = 8;
+						byte[] buffer = new byte[length];
+						if (length != 8) Console.Write("【!!!】");
+						int result = br.Read(buffer, 0, buffer.Length);
+						ulong plane = BitConverter.ToUInt64(buffer, 0);
+						ulong decrypt_value = Key.Decrypt(plane);
+						Console.Write($"Pos : {br.BaseStream.Position.ToString().PadLeft(3, '0')}, IN : {plane.ToString().PadLeft(20, '0')}({plane.ToString("x").PadLeft(16, '0')}), Dec : {decrypt_value.ToString().PadLeft(20, '0')}, 0x : {decrypt_value.ToString("x").PadLeft(16, '0')}, Export : ");
+						foreach (byte b in BitConverter.GetBytes(decrypt_value)) {
+							Console.Write(Convert.ToString(b, 16).PadLeft(2, '0'));
+							file.Write(b);
+						}
+						Console.WriteLine("");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ cryptosystem/encrypt/EncryptMain.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+/*
+ * カレントディレクトリ取得
+ * http://var.blog.jp/archives/66978870.html
+ * 文字コード判別
+ * http://namco.hatenablog.jp/entry/2017/02/05/160119
+ * バイナリファイルのIO
+ * https://qiita.com/miyamoto_works/items/637732d880defd1d437d
+ * 暗号化参考
+ * http://www.mizuko.okayama-c.ed.jp/gakka/i/H22kaken/H22h-ang.pdf
+ * 共通鍵にした裏付け
+ * https://www.nic.ad.jp/ja/materials/iw/2008/proceedings/H10/IW2008-H10-01.pdf
+ */
+
+namespace encrypt {
+
+	/// <summary>
+	///	暗号化を行うメインクラス
+	/// </summary>
+	public class EncryptMain {
+
+
+		/// <summary>
+		/// 暗号化を行うメイン関数
+		/// </summary>
+		/// <param name="args">コマンドライン引数</param>
+		public static void Main (string[] args) {
+
+			// コンソールの文字コードの設定
+			Console.OutputEncoding = Encoding.UTF8;
+			// コマンドライン引数で指定されたファイルの暗号化を行う
+			BinFileEncrypt(args[0]);
+		}
+
+
+		/// <summary>
+		/// バイナリファイルの暗号化を行う
+		/// </summary>
+		/// <param name="filePath">暗号化する対象のファイルのパス</param>
+		static void BinFileEncrypt (string filePath) {
+
+			/*
+			 * バイナリファイルの読み込みを行う
+			 * 現状はカレントディレクトリのバイナリファイルでやる
+			 */
+			using (BinaryReader br = new BinaryReader(File.OpenRead(filePath))) {
+
+				// 暗号化ファイルの出力バス
+				string exportFilePath = $"{filePath}.enc";
+
+				// すでに暗号化されたファイルが存在している場合は削除する
+				if (File.Exists(exportFilePath)) File.Delete(exportFilePath);
+
+				// バイナリファイルとして書き込む
+				using (BinaryWriter file = new BinaryWriter(new FileStream(exportFilePath, FileMode.CreateNew))) {
+
+					// 読み込めている間コンソールに出力し続ける
+					while (br.BaseStream.Position != br.BaseStream.Length) {
+
+						// バイナリファイルから8バイト取得するための変数を準備
+						byte[] buffer = new byte[8];
+						// 実際に読み込みを行う
+						int result = br.Read(buffer, 0, 8);
+						// 読み込んだ鍵をulong型にキャスト
+						ulong plane = BitConverter.ToUInt64(buffer, 0);
+
+						// 暗号化する
+						ulong encrypt_value = Key.Encrypt(plane);
+						Console.WriteLine($"Pos : {br.BaseStream.Position.ToString().PadLeft(3, '0')}, IN : {plane.ToString().PadLeft(20, '0')}, Enc : {encrypt_value.ToString().PadLeft(20, '0')}, Ex : {encrypt_value.ToString("x").PadLeft(16, '0')}");
+						// 暗号化された値の書き込みを行う
+						file.Write(encrypt_value);
+					}
+				}
+				Console.WriteLine("");
+				Console.WriteLine("");
+				Console.WriteLine($"exportPath:{filePath}");
+			}
+		}
+	}
+}
